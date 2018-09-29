92945fe9db7a4b005569dd541f4b34aad046ca9d
zazazabo <z277402131@126.com>
2018-09-29 18:28:04 +0800

    11 Signed-off-by: zazazabo <z277402131@126.com>

--- testiocp/resource.h
+++ testiocp/resource.h
@@ -4,6 +4,7 @@
 //
 #define IDD_DIALOG1                     101
 #define IDR_ZIP1                        103
+#define IDI_ICON1                       104
 #define IDC_BUTTON1                     1000
 #define IDC_BUTTON2                     1001
 #define IDC_LIST1                       1002
@@ -21,7 +22,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
